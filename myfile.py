e5b8bf07fd64c8f439879170406f3e742e70859a
Jorge Chirinos <jorge.chirinos@upch.pe>
2023-06-16 22:24:24 -0500

    Probando actualizar archivos con git

--- myfile.py
+++ myfile.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Casos Positivos Covid')
+st.write('Aquí revisaremos los casos positivos de covid en Perú a lo largo del tiempo.')
 
 #df = pd.read_csv('positivos_covid.csv')
 #st.line_chart(df)
