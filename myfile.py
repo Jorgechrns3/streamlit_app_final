fa2d25ee1d4aca93dd18d4ae6aabc49f7d33c55a
ZULY781 <137363144+ZULY781@users.noreply.github.com>
2023-06-22 00:34:13 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -9,3 +9,18 @@ st.write('Aquí revisaremos los casos positivos de covid en Perú a lo largo del
 #st.line_chart(df)
 date=st.date_input('Seleccionar fecha')
 
+import streamlit as st
+import pandas as pd
+import gdown
+
+#id = 1op-iq0XhBXBQOPlagCPE9TzFsFkkNVjQ
+@st.experimental_memo
+def download_data():
+  #https://drive.google.com/uc?id=
+  url = "https://drive.google.com/uc?id=1op-iq0XhBXBQOPlagCPE9TzFsFkkNVjQ"
+  output= "data.csv"
+  gdown.download(url,output,quiet = False)
+  
+download_data()
+data = pd.read_csv("data.csv", sep = ";", parse_dates = ["FECHA_CORTE","FECHA_RESULTADO"])
+Simplificado = data.drop(columns = ["DISTRITO","FECHA_CORTE","FECHA_RESULTADO","UBIGEO","id_persona"])
