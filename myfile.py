644352c309b47c569557c141b022cc989d9e1d2c
ZULY781 <137363144+ZULY781@users.noreply.github.com>
2023-06-29 15:35:32 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Casos Positivos Covid')
 st.header('Total de Casos:')
@@ -32,5 +34,54 @@ formatted_count = "{:,}".format(total)
 # Mostrar el conteo formateado
 count_display = f"<h1 style='text-align: center;'>{formatted_count}</h1>"
 st.markdown(count_display, unsafe_allow_html=True)
+
+#Sistema de filtros
+
+#Construccion del set/list de departamentos (Valores unicos sin NA)
+set_departamentos = np.sort(df['DEPARTAMENTO'].dropna().unique())
+#Seleccion del departamento
+opcion_departamento = st.selectbox('Selecciona un departamento', set_departamentos)
+df_departamentos = df[df['DEPARTAMENTO'] == opcion_departamento]
+num_filas = len(df_departamentos.axes[0]) 
+
+#Construccion del set/list de provincias (Valores unicos sin NA)
+set_provincias = np.sort(df_departamentos['PROVINCIA'].dropna().unique())
+#Seleccion de la provincia
+opcion_provincia = st.selectbox('Selecciona una provincia', set_provincias)
+df_provincias = df_departamentos[df_departamentos['PROVINCIA'] == opcion_provincia]
+num_filas = len(df_provincias.axes[0]) 
+
+#Construccion del set/list de distritos (Valores unicos sin NA)
+set_distritos = np.sort(df_departamentos['DISTRITO'].dropna().unique())
+#Seleccion de la distrito
+opcion_distrito = st.selectbox('Selecciona un distrito', set_distritos)
+df_distritos = df_departamentos[df_departamentos['DISTRITO'] == opcion_distrito]
+num_filas = len(df_distritos.axes[0]) 
+
+st.write('Numero de registros:', num_filas)
+
+#Gráficas
+
+#Gráfica de pie de METODODX
+df_metododx = df_distritos.METODODX.value_counts()
+df_metododx = pd.DataFrame(df_metododx)
+df_metododx = df_metododx.reset_index()  
+df_metododx.columns = ['METODODX', 'Total']
+
+fig1, ax1 = plt.subplots()
+ax1.pie(df_metododx['Total'], labels=df_metododx['METODODX'], autopct='%1.1f%%')
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.write('Distribución por METODODX:')
+st.pyplot(fig1)
+
+#Gráfica de barras de SEXO
+df_SEXO = df_distritos.SEXO.value_counts()
+st.write('Distribución por SEXO:')
+st.bar_chart(df_SEXO)
+
+#Gráfica de barras de EDAD
+df_edad = df_distritos.EDAD.value_counts()
+st.write('Distribución por EDAD:')
+st.bar_chart(df_edad)
 
 
